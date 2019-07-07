dcceb255e207d4f8fa79dffb1f3c157ab7718af9
Ken <kenofuniverse@gmail.com>
2019-07-07 22:42:20 +0800

    update build version

---
+++ docs/precache-manifest.a246f71530f780e753efe8893902fe9e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9e3f033f912c06f670f1c4b59e1a437",
+    "url": "/challenge/index.html"
+  },
+  {
+    "revision": "a7ae690e8af0b56e7221",
+    "url": "/challenge/static/css/main.fd6202b6.chunk.css"
+  },
+  {
+    "revision": "57cf11bcd97f14e7854c",
+    "url": "/challenge/static/js/2.116f4e1a.chunk.js"
+  },
+  {
+    "revision": "a7ae690e8af0b56e7221",
+    "url": "/challenge/static/js/main.544a77c2.chunk.js"
+  },
+  {
+    "revision": "fdb5274d4975753ce6dd",
+    "url": "/challenge/static/js/runtime~main.d5b78ddd.js"
+  }
+]);
