1c2b366a7023956a50bcf82b470f8c2990bbff6d
Ken <kenofuniverse@gmail.com>
2019-07-07 22:35:24 +0800

    add to github page

---
+++ docs/precache-manifest.5c5986988ab367811ac55f82be354f3d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c2da7a40a3995ef4ed2f8d8f12f7147",
+    "url": "/index.html"
+  },
+  {
+    "revision": "53a1e95df6b1841f2506",
+    "url": "/static/css/main.fd6202b6.chunk.css"
+  },
+  {
+    "revision": "57cf11bcd97f14e7854c",
+    "url": "/static/js/2.116f4e1a.chunk.js"
+  },
+  {
+    "revision": "53a1e95df6b1841f2506",
+    "url": "/static/js/main.c27326b1.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  }
+]);
